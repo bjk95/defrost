b29af2eb3cb2d2fbe37fb1838ff30c36c2f61240
Brad Kester <bradley@canva.com>
2026-04-30 00:21:11 +1000

    feat: register pytest adapter alongside Go adapter

--- exec.go
+++ exec.go
@@ -5,12 +5,14 @@ import (
 	"os"
 
 	"github.com/bjk95/defrost/internal/golang"
+	"github.com/bjk95/defrost/internal/python/pytest"
 	"github.com/bjk95/defrost/internal/runner"
 )
 
 func HandleExecution(cmd []string) {
 	reg := runner.NewRegistry()
 	reg.Register(golang.Adapter{})
+	reg.Register(pytest.Adapter{})
 
 	a := reg.Find(cmd)
 	if a == nil {
